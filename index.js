e0cbd67a8e985e1ba840ea8cd5996eb59c9d2bdb
Md. Moinul Hassan <moinulh.msn@gmail.com>
2022-11-09 22:40:19 +0600

    added some API

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.get('/homeservices', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
 
     }
     catch(err){
