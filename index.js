efe09bfb7bde2f184d96ff9f0f14959ebe76e8b7
Md. Moinul Hassan <moinulh.msn@gmail.com>
2022-11-09 19:42:50 +0600

    added services

--- index.js
+++ index.js
@@ -7,10 +7,33 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.nhrod4k.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const serviceCollection = client.db('ServiceData').collection('Services')
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    catch(err){
+         console.log(err);
+    }
+    finally{
+
+    }
+}
+
+run().catch(e => console.error*(e));
 
 app.get('/', (req, res) => {
     res.send('service server is running')
